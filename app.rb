421d5513d1336b5ce43c2f6d2f7c329be953cccc
RSijelmass <R.Sijelmass@gmail.com>
2017-05-24 15:07:42 +0100

    Create new path '/names' - create index.erb with form - create and pass feature tests

--- app.rb
+++ app.rb
@@ -3,7 +3,13 @@ require 'sinatra/base'
 class Battle < Sinatra::Base
 
   get '/' do
-    'Testing infrastructure working!'
+    erb :index
+  end
+
+  post '/names' do
+    @name1 = params[:name1]
+    @name2 = params[:name2]
+    erb :play
   end
 
   get '/battle' do
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,11 +1,16 @@
 feature 'Testing infrastructure' do
-  scenario 'Can run app and check page content' do
-    visit ('/')
-    expect(page).to have_content 'Testing infrastructure working!'
-  end
-
   scenario 'Can check on the battle page and check its content' do
     visit('/battle')
     expect(page).to have_content('Hello Battle')
+  end
+
+  scenario 'Players can submit their names in a form' do
+    visit ('/')
+    fill_in('name1', with: 'Guillermo')
+    fill_in('name2', with: 'Rita')
+    click_button('Submit')
+    expect(page).to have_text('Guillermo')
+    expect(page).to have_content('Rita')
+    expect(page).to have_content('vs')
   end
 end
